320a0a0d8e184ac89d40f176c5607fe86b819f2a
Stayu Kasabov <stayu.kasabov@gmail.com>
2026-03-14 21:04:07 +0200

    Revert "fix: remove pynput/brotli from hard deps; lazy-import pynput"
    This reverts commit bc1721c3635f171197ce9f90e4453caef8e2131e.

--- capcat/__init__.py
+++ capcat/__init__.py
@@ -1,4 +1,4 @@
 """Capcat — A command-line tool designed to solve content preservation challenges with Ethical Scraping."""
 
-__version__ = "1.0.20"
+__version__ = "1.0.19"
 __all__ = ["__version__"]
