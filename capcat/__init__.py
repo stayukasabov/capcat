dce62bed3d645e68b6ab9a80312b0055d6bbe569
xmac <xmac@xmacs-Mac-mini.local>
2026-03-14 13:03:06 +0200

    fix: align header breadcrumb and h1 to content column

--- capcat/__init__.py
+++ capcat/__init__.py
@@ -1,4 +1,4 @@
 """Capcat — A command-line tool designed to solve content preservation challenges with Ethical Scraping."""
 
-__version__ = "1.0.13"
+__version__ = "1.0.14"
 __all__ = ["__version__"]
