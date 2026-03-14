ee2da58768786ec4b8fa199e013fe65d665dfd55
Stayu Kasabov <stayu.kasabov@gmail.com>
2026-03-14 22:15:57 +0200

    fix: expand bundle names to source IDs for --all and bundle all

--- capcat/__init__.py
+++ capcat/__init__.py
@@ -1,4 +1,4 @@
 """Capcat — A command-line tool designed to solve content preservation challenges with Ethical Scraping."""
 
-__version__ = "1.0.27"
+__version__ = "1.0.28"
 __all__ = ["__version__"]
