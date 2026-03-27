1d51e26fb976dd44cdf4ba31f276af52967214f4
Stayu Kasabov <stayu.kasabov@gmail.com>
2026-03-27 03:55:15 +0200

    bump: version to 1.5.9

--- capcat/__init__.py
+++ capcat/__init__.py
@@ -1,6 +1,6 @@
 """Capcat — A command-line tool designed to solve content preservation challenges with Ethical Scraping."""
 
-__version__ = "1.5.8"
+__version__ = "1.5.9"
 
 from capcat.cli import run_app  # noqa: E402
 
